53548805805403de270ab3c0e254e5e361a87a20
李佳程 <2893438107@qq.com>
2023-07-26 14:59:51 +0800

    用java创建一个zk节点

--- src/main/test/org/example/Test.java
+++ src/main/test/org/example/Test.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Before;
+
 public class Test {
+  private final String LOCAL_HOST="127.0.0.1";
+  private final String ZK_PORT="2181";
+
+  private ZooKeeper zooKeeper;
+  @Before
+  public void initZK(){
+    try {
+      zooKeeper=new ZooKeeper(LOCAL_HOST+":"+ZK_PORT,5000,null);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   @org.junit.Test
   public void test(){
+    try {
+      System.out.println(
+          "zooKeeper.create(\"/ljc/hello\",\"Hello ZK\".getBytes(StandardCharsets.UTF_8),null,\n          CreateMode.PERSISTENT) = "
+              + zooKeeper.create("/ljc/hello", "Hello ZK".getBytes(StandardCharsets.UTF_8), Ids.OPEN_ACL_UNSAFE,
+              CreateMode.PERSISTENT));
+    } catch (KeeperException e) {
+      throw new RuntimeException(e);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
