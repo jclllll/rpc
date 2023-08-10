07134b765929063e32ebbd2f0518557efff2a73b
李佳程 <2893438107@qq.com>
2023-08-10 17:31:16 +0800

    加入雪花算法

--- LRPC-UidGenerator/src/main/java/com/lrpc/IdGenerator.java
+++ LRPC-UidGenerator/src/main/java/com/lrpc/IdGenerator.java
@@ -6,21 +6,55 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class IdGenerator {
-
-
     private static long lastTime = -1;
     private static long currentTime = 0;
-    private static int seq = 0;
+    private static long seq = 0;
+    private static int myRoom;
+    private static int myMachine;
 
-    public static long getUid(int room, int machine) {
-        if(room>GeneratorConstant.MAX_ROOM || machine>GeneratorConstant.MAX_MACHINE){
+    public static void init(int room, int machine) {
+        myRoom = room;
+        myMachine = machine;
+    }
+
+    public static long getUid() {
+        if (myRoom == 0 || myMachine == 0) {
+            throw new IdException("没有绑定所在机房及机器号");
+        }
+        return getUid(myRoom,myMachine);
+    }
+
+    synchronized public static long getUid(int room, int machine) {
+        if (room > GeneratorConstant.MAX_ROOM || machine > GeneratorConstant.MAX_MACHINE) {
             throw new IdException("机房或机器超过最大限制");
         }
         currentTime = getTime();
-        long id = (currentTime << GeneratorConstant.TIME_STAMP_LEFT_MOVE);
-        id |= id | (room << GeneratorConstant.ROOM_LEFT_MOVE);
-        id |= id | (machine << GeneratorConstant.MACHINE_LEFT_MOVE);
-        id |= id | seq;
+        if (currentTime < lastTime) {
+            if (lastTime - currentTime >= 1000000) {
+                throw new IdException("时钟回拨时间过长");
+            } else {
+                while (currentTime < lastTime) {
+                    currentTime = getTime();
+                }
+                seq = 0;
+            }
+        }
+        long id = 0;
+        if (currentTime == lastTime) {
+            if (seq >= GeneratorConstant.MAX_SEQ) {
+                while (currentTime == lastTime) {
+                    currentTime = getTime();
+                }
+                seq = 0;
+            }
+        } else {
+            seq = 0;
+        }
+        id = (currentTime << GeneratorConstant.TIME_STAMP_LEFT_MOVE);
+        id |= ((long) room << GeneratorConstant.ROOM_LEFT_MOVE);
+        id |= ((long) machine << GeneratorConstant.MACHINE_LEFT_MOVE);
+        id |= seq++;
+        lastTime = currentTime;
         return id;
     }
 
@@ -40,25 +74,21 @@ public class IdGenerator {
         return System.currentTimeMillis() - startTime;
     }
 
-    public static void main(String[] args) throws ParseException {
-    }
-    public static void print(int num){
-        for(int i=31;i>=0;i--){
-            if(((num>>i)&1)==1){
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            if (((num >> i) & 1) == 1) {
                 System.out.print("1");
-            }
-            else{
+            } else {
                 System.out.print("0");
             }
         }
         System.out.println();
     }
-    public static void print(long num){
-        for(int i=63;i>=0;i--){
-            if(((num>>i)&1)==1){
+    public static void print(long num) {
+        for (int i = 63; i >= 0; i--) {
+            if (((num >> i) & 1) == 1) {
                 System.out.print("1");
-            }
-            else{
+            } else {
                 System.out.print("0");
             }
         }
